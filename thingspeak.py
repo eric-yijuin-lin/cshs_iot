de82b19262ba6d92cf5d5b0ef80acc06a17921de
eric-yijuin-lin <63579803+eric-yijuin-lin@users.noreply.github.com>
2022-12-15 10:29:37 +0800

    Update thingspeak.py

--- thingspeak.py
+++ thingspeak.py
@@ -3,8 +3,8 @@ import urequests as requests
 import time
 
 # 設定 wifi SSID 名稱、wifi 密碼與伺服器網址
-WIFI_SSID = 'art'
-WIFI_PASSWORD = '2643344-250'
+WIFI_SSID = ''
+WIFI_PASSWORD = ''
 
 # 建立 wifi 連線
 wifi = network.WLAN(network.STA_IF)
@@ -19,11 +19,6 @@ while True:
         break
     
 # 發送 http 請求
-SERVER_URL = '你的 ThingSpeak 網址（記得改查詢參數）'
-resp = requests.get(url= SERVER_URL)
-if resp.status_code == 200:
-    print(resp.text)
-else:
-    print('status_code', resp.status_code)
+
 
 
